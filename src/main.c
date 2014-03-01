790a1e0dfda915be23b9efd3c72c4e197d21a13a
Kurt Cancemi <kurt@x64Architecture.com>
2014-03-01 11:09:32 -0500

    Simplified code

--- src/main.c
+++ src/main.c
@@ -30,6 +30,7 @@ BOOL IsWindows7OrGreater()
 int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 	LPSTR lpCmdLine, int nCmdShow)
 {
+	LPCSTR message;
 	char profilepath[250];
 	ExpandEnvironmentStrings("%USERPROFILE%", profilepath, 250);
 
@@ -45,53 +46,33 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 	{
 		RegisterHotKey(NULL, 1, MOD_CONTROL | MOD_ALT | MOD_NOREPEAT, 0x54); // 0x54 = T key
 		RegisterHotKey(NULL, 2, MOD_CONTROL | MOD_ALT | MOD_NOREPEAT, 0x51); // 0x51 = Q key
-		MessageBox(NULL,
-			(LPCSTR)"HotKey 'CTRL+ALT+T (Start CMD), CTRL+ALT+Q (Quit)' registered, using MOD_NOREPEAT flag.\n",
-			(LPCSTR)"Notice",
-			MB_ICONINFORMATION | MB_OK);
-		while (GetMessage(&msg, NULL, 0, 0) != 0)
-		{
-			if (msg.message == WM_HOTKEY)
-			{
-				//printf("WM_HOTKEY received\n");
-				switch (msg.wParam) // Switch HotKey id
-				{
-				case 1: // HotKey 1
-					ShellExecuteEx(&shExInfo);
-					break;
-				case 2: // HotKey 2
-					UnregisterHotKey(NULL, 1);
-					UnregisterHotKey(NULL, 2);
-					exit(0);
-					break;
-				}
-			}
-		}
+		message = "HotKey 'CTRL+ALT+T (Start CMD), CTRL+ALT+Q (Quit)' registered, using MOD_NOREPEAT flag.\n";
 	}
 	else
 	{
 		RegisterHotKey(NULL, 1, MOD_CONTROL | MOD_ALT, 0x54); // 0x54 = T key
 		RegisterHotKey(NULL, 2, MOD_CONTROL | MOD_ALT, 0x51); // 0x51 = Q key
-		MessageBox(NULL,
-			(LPCSTR)"HotKey 'CTRL+ALT+T (Start CMD) & CTRL+ALT+Q (Quit)' registered.\n",
-			(LPCSTR)"Notice",
-			MB_ICONINFORMATION | MB_OK);
-		while (GetMessage(&msg, NULL, 0, 0) != 0)
+		message = "HotKey 'CTRL+ALT+T (Start CMD), CTRL+ALT+Q (Quit)' registered.\n";
+	}
+	MessageBox(NULL,
+		message,
+		(LPCSTR)"Notice",
+		MB_ICONINFORMATION | MB_OK);
+	while (GetMessage(&msg, NULL, 0, 0) != 0)
+	{
+		if (msg.message == WM_HOTKEY)
 		{
-			if (msg.message == WM_HOTKEY)
+			//printf("WM_HOTKEY received\n");
+			switch (msg.wParam) // Switch HotKey id
 			{
-				//printf("WM_HOTKEY received\n");
-				switch (msg.wParam) // Switch HotKey id
-				{
-				case 1: // HotKey 1
-					ShellExecuteEx(&shExInfo);
-					break;
-				case 2: // HotKey 2
-					UnregisterHotKey(NULL, 1);
-					UnregisterHotKey(NULL, 2);
-					exit(0);
-					break;
-				}
+			case 1: // HotKey 1
+				ShellExecuteEx(&shExInfo);
+				break;
+			case 2: // HotKey 2
+				UnregisterHotKey(NULL, 1);
+				UnregisterHotKey(NULL, 2);
+				exit(0);
+				break;
 			}
 		}
 	}
